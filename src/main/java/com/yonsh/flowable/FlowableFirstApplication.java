83139ed9668554c767dfb6520e557f55f55577b4
yonsh.xu <yonsh.xu@gmail.com>
2023-10-10 22:52:39 +0800

    init

---
+++ src/main/java/com/yonsh/flowable/FlowableFirstApplication.java
@@ -0,0 +1,13 @@
+package com.yonsh.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableFirstApplication.class, args);
+	}
+
+}
