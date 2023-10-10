bd38274beda236333d5d910c4ec50f9c8a7b31c7
yonsh.xu <yonsh.xu@gmail.com>
2023-10-10 23:20:57 +0800

    添加flowable

--- src/main/java/com/yonsh/flowable/FlowableFirstApplication.java
+++ src/main/java/com/yonsh/flowable/FlowableFirstApplication.java
@@ -3,7 +3,7 @@ package com.yonsh.flowable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class FlowableFirstApplication {
 
 	public static void main(String[] args) {
