af5f79f3e66c5671e2ac6272cc48ee04e1e32f0a
yonsh.xu <yonsh.xu@gmail.com>
2023-10-10 23:58:46 +0800

    添加xml

--- src/main/java/com/yonsh/flowable/FlowableFirstApplication.java
+++ src/main/java/com/yonsh/flowable/FlowableFirstApplication.java
@@ -1,7 +1,13 @@
 package com.yonsh.flowable;
 
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+
+import org.flowable.engine.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(proxyBeanMethods = false)
 public class FlowableFirstApplication {
@@ -10,4 +16,22 @@ public class FlowableFirstApplication {
 		SpringApplication.run(FlowableFirstApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner init(final RepositoryService repositoryService,
+								  final RuntimeService runtimeService,
+								  final TaskService taskService) {
+
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... strings) throws Exception {
+				System.out.println("Number of process definitions : " + repositoryService.createProcessDefinitionQuery().count());
+				System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+
+				runtimeService.startProcessInstanceByKey("oneTaskProcess");
+
+				System.out.println("Number of tasks after process start: " + taskService.createTaskQuery().count());
+			}
+		};
+	}
 }
